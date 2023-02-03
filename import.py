076c912c092aebe3a7988b71017690b7ffaff28e
Austin John <austinjohn@tamu.edu>
2023-02-02 22:05:39 -0600

    python file to import data from URL and write it into new text file

---
+++ import.py
@@ -0,0 +1,12 @@
+import urllib.request
+
+textfile = urllib.request.urlopen("https://s3.amazonaws.com/tcmg476/http_access_log")
+data = open("DataSet.txt", "w+")
+
+for line in textfile:
+    strline = str(line)
+    data.write(strline + '\n' )
+
+data.close()
+
+#the above portion imports the data from the webserver into a new .txt file called DataSet. The for loop prints out each new log into a new line to seperate logs
