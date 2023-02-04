c8792d2047bbd059f736edda59acb98f531fdea5
Austin John <austinjohn@tamu.edu>
2023-02-04 15:50:34 -0600

    .py script to read DataSet and print requests || v2

--- Austin-project3.py
+++ Austin-project3.py
@@ -10,17 +10,36 @@ while True:
     line = d.readline()
 
     if not line:
-        print(counter1)
         break
 
 # while loop to count all the lines in the text file, as each line represents an individual log. Stores the count in the variable counter1
 
-counter1 = counter1 - 1
+counter1 -= 1
 
 # the DataSet.txt file has one extra blank line at the end of the file which does not contain a log.     
 
 print("Total requests made in the entire time period of the log: ", counter1)
 
-print("Total requests made in the last 6 months: ")
+
+# the last log was made on 11/Oct/1995:14:14:17
+# 6 months prior to this would be 11/Apr/1995:14:14:17
+# the earliest log file within the last 6 months occured on 11/Apr/1995:14:14:19
+
+counter2 = 0
+
+date = '[11/Apr/1995:14:14:19 -0600]'
+with open(r'DataSet.txt', 'r') as file:
+    logs = file.readlines()
+    for log in logs:
+
+        if log.find(date) != -1:
+
+            print ('Total requests made in the last 6 months is: ', counter1 - logs.index(log) - 1)
+            # -1 is to count the log that gets excluded, as we subtract the counter in this for loop from the counter1 value
+           
+
+
+
+
 
 
