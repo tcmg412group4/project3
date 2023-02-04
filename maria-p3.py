2aef1f9046ce993f11693073e2def9e78a52a864
mariasuchil <94920328+mariasuchil@users.noreply.github.com>
2023-02-04 16:38:49 -0600

    Update maria-p3.py
    still working on it

--- maria-p3.py
+++ maria-p3.py
@@ -1,1 +1,14 @@
 
+# trying to code the 6 months data
+from datetime import date
+
+count = 0
+
+max_date = 
+min_date = 
+with open('DataSet.txt') as data: # open file and read data
+    for line in data: # read each line in the txt file
+         # scan for the bast 6 months
+        count += 1
+
+print("Total requests made in the last 6 months: ", line)
