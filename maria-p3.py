9be57e266ee59ac7997edf94c2188bfe72212516
mariasuchil <94920328+mariasuchil@users.noreply.github.com>
2023-02-05 11:09:58 -0600

    Update maria-p3.py
    the top chunk of code works for total requests.

--- maria-p3.py
+++ maria-p3.py
@@ -1,14 +1,39 @@
 
+# count the total requests
+
+with open(r"DataSet.txt", 'r') as tlines:
+    x = len(tlines.readlines())
+    print('Total lines:', x)
+
+
+
+
+
+
 # trying to code the 6 months data
-from datetime import date
+
+from datetime import datetime
+
+
+date = '[11/Oct/1995:14:14:17 -0600]'
+i = 0
 
 count = 0
 
-max_date = 
-min_date = 
-with open('DataSet.txt') as data: # open file and read data
-    for line in data: # read each line in the txt file
-         # scan for the bast 6 months
-        count += 1
+with open('DataSet.txt','r') as data: # open file and read data
+    if data.mode =='r':
+    contents = data.readlines() # read each line in the txt file
 
-print("Total requests made in the last 6 months: ", line)
+        # scan for the past 6 months
+    for contents in data:
+        sixmonths = contents.strftime("%d, %B, %y")
+        for i in sixmonths:
+            count += 1
+        if not date:
+            break
+        break
+
+
+print("Total requests made in the last 6 months: ", count)
+
+data.close()
